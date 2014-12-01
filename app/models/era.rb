0ae4723762ed7e408251272dc95c06ba443c762e
Graeme Mathieson <mathie@woss.name>
2014-12-01 14:13:07 +0000

    End dates must be after start dates.

--- app/models/era.rb
+++ app/models/era.rb
@@ -3,5 +3,15 @@ class Era < ActiveRecord::Base
 
   validates :title, presence: true
   validates :started_on, presence: true
-  validates :ended_on, presence: true
+  validates :ended_on, presence: true, after: true
+
+  validate :must_end_after_start
+
+  private
+
+  def must_end_after_start
+    if started_on.present? && ended_on.present? && ended_on < started_on
+      errors.add(:ended_on, 'must be after started on')
+    end
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,5 +5,15 @@ class Event < ActiveRecord::Base
   validates :started_on, presence: true
   validates :title, presence: true
 
+  validate :must_end_after_start
+
   mount_uploader :image, EventImageUploader
+
+  private
+
+  def must_end_after_start
+    if started_on.present? && ended_on.present? && ended_on < started_on
+      errors.add(:ended_on, 'must be after started on')
+    end
+  end
 end
