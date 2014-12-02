02542d3c45012837d3145ab79778d0df6544d92f
Graeme Mathieson <mathie@woss.name>
2014-12-02 09:52:11 +0000

    Don't configure fog if the env vars are missing.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,13 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-  }
-  config.fog_directory = "mathie-timelines-#{Rails.env}"
-  config.storage = :fog
+  if ENV['AWS_ACCESS_KEY_ID'].present?
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    config.fog_directory = "mathie-timelines-#{Rails.env}"
+    config.storage = :fog
+  else
+    config.storage = :file
+  end
 end
