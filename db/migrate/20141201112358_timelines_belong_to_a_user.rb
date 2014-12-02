83f608b009f2df03766ca5185a894b046d4d3772
Graeme Mathieson <mathie@woss.name>
2014-12-02 09:59:40 +0000

    Cope with migrating when there are no users.
    And, fortunately, no timelines either.

--- db/migrate/20141201112358_timelines_belong_to_a_user.rb
+++ db/migrate/20141201112358_timelines_belong_to_a_user.rb
@@ -1,12 +1,19 @@
 class TimelinesBelongToAUser < ActiveRecord::Migration
   def change
-    change_table :timelines do |t|
-      t.uuid :user_id, null: false, default: User.first.id
+    if User.count > 0
+      change_table :timelines do |t|
+        t.uuid :user_id, null: false, default: User.first.id
+      end
+
+      change_column_default :timelines, :user_id, nil
+    else
+      change_table :timelines do |t|
+        t.uuid :user_id, null: false
+      end
     end
 
     add_index :timelines, :user_id
     add_foreign_key :timelines, :users, on_delete: :restrict, on_update: :restrict
 
-    change_column_default :timelines, :user_id, nil
   end
 end
