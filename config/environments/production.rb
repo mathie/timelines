f4af28ac6d9b77cbb7151e7504d5c5d62849c8d7
Graeme Mathieson <mathie@woss.name>
2014-12-03 09:01:58 +0000

    SSL for mailer URLs."

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'personal-timelines.herokuapp.com' }
+  config.action_mailer.default_url_options = {
+    protocol: 'https',
+    host: 'personal-timelines.herokuapp.com'
+  }
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
