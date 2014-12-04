d0b18ab7fa5d1a8f8f16f85e6658ef5d0d3bf48c
Graeme Mathieson <mathie@woss.name>
2014-12-04 14:13:11 +0000

    Flash notice on login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     @user = Authenticator.find_or_create_by_auth_hash(session_params)
     self.current_user = @user
-    redirect_to session.delete(:return_url) || root_path
+    redirect_to session.delete(:return_url) || root_path, notice: 'You have been signed in.'
   end
 
   def destroy
